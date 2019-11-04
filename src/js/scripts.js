d38c0483e90a43303dec52e6231f80ac4b719c33
Owleek Owleek <owleek@mail.ru>
2019-11-04 15:05:26 +0300

    mobile-noJS

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+    const $catalogueMenuTitle = $(".catalogueMenu__title");
+    
+    if($(window).width() < 1200) {
+        $(".catalogueMenu__title").html("Каталог товаров");
+    } else {
+        $(".catalogueMenu__title").html("Все разделы каталога")
+    }
+
+    $( window ).resize(function() {
+        if($(window).width() < 1200) {
+            $(".catalogueMenu__title").html("Каталог товаров");
+        } else {
+            $(".catalogueMenu__title").html("Все разделы каталога")
+        }
+    });
 
 });
